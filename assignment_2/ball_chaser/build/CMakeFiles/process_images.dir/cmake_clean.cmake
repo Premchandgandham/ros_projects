3d16b6908fba695c230afb1b8e36b0f0ccbfbaa2
ros <g.premchand5030@gmail.com>
2021-02-15 17:03:23 +0530

    updated

---
+++ assignment_2/ball_chaser/build/CMakeFiles/process_images.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/process_images.dir/src/process_images.cpp.o"
+  "devel/lib/ball_chaser/process_images"
+  "devel/lib/ball_chaser/process_images.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/process_images.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- assignment_2/ball_chaser/src/process_images.cpp
+++ assignment_2/ball_chaser/src/process_images.cpp
@@ -2,7 +2,7 @@
 #include "ball_chaser/DriveToTarget.h"
 #include <sensor_msgs/Image.h>
 
-
+int k = 0;
 ros::ServiceClient client;
 
 void drive_robot(float lin_x, float ang_z)
@@ -18,57 +18,114 @@ void drive_robot(float lin_x, float ang_z)
         ROS_ERROR("Failed to call service safe_move");
 }
 
+
+
 void process_image_callback(const sensor_msgs::Image img)
 {
-
+  
+  
+  
   int white_pixel = 255;
   bool found_ball=false;
-  int ball_x,ball_y;
   int bpp=img.step/img.width;
+  int ball_x,ball_y;  
   float lin_x,ang_z;
-
   
-  found_ball=false;
-  for (int y = 0; y < img.height; y++) {
+  ROS_INFO("Height(rows) = %d",img.height);
+  ROS_INFO("wodth(colums) = %d",img.width);
+  ROS_INFO("Step value = %d",img.step);
+  ROS_INFO("actual matrix size %d",img.height*img.step);
+  
+ found_ball=false;
+
+   for (int y = 0; y < img.height; y++) {
     for(int x=0; x < img.width; x++) {
       if (img.data[y*img.step+x*bpp] == white_pixel) {
         found_ball=true;
         ball_x=x;ball_y=y;
+        ROS_INFO("width at which ball places %d",x);
+        ROS_INFO("height at which ball places %d",y);
         break;
       }
     }
   }
   
-  if(found_ball) {
-    ROS_INFO_STREAM("ball_x: " + std::to_string(ball_x));
-    if(ball_x < 266)
-    {
-      ROS_INFO_STREAM("ball in the left 1/3");
-      lin_x=0.1;
-      ang_z=0.01*(400-ball_x);
-    } 
-    else
-      if(ball_x >= 266 && ball_x < 533)
-      {
-        ROS_INFO_STREAM("ball in the middle section");
-        lin_x=0.5;
-        if(ball_x<400)
-          ang_z=0.01*(400-ball_x);
-        else
-          ang_z=-0.01*(ball_x-400);
-        
-      }
-      else
-        if(ball_x >= 533) 
-        {
-          ROS_INFO_STREAM("ball in the right section");
-          lin_x=0.1;
-          ang_z=-0.01*(ball_x-400);
-        }
+// Height is no of rows
+// width is  no of colums
+
+  
+if(k < 5){
+  
+if(found_ball) {
+	
+	ROS_INFO("Turn value = %d",k);
+
+    
+     
+    	ROS_INFO_STREAM("ball_x: " + std::to_string(ball_x));
+    		if(ball_x < 400)
+   			 {
+			      ROS_INFO_STREAM("ball in the left 1/3");
+			      lin_x=0.0;
+			      ang_z=0.005*(400-ball_x);
+			      k++;
+			      ROS_INFO("turn value = %d",k);
+			      
+    			} 
+
+      		else if(ball_x >= 400)
+        	 
+			{
+			  ROS_INFO_STREAM("ball in the right section");
+			  lin_x=0.0;
+			  ang_z=-0.005*(ball_x-400);
+			  k++;
+			  ROS_INFO("turn value = %d",k);
+			  
+			}
+		
+       
+
     drive_robot(lin_x, ang_z);
-  } else drive_robot(0, -1.5);
+    
+    
+  }
+  
+  
+ else
+ 	{
+  		drive_robot(0.0,0.0);
+  		ROS_INFO("No ball found");
+  		k = 0;
+  		
+  	}
+  
 }
 
+
+
+
+       else
+       	{
+       
+        		lin_x = 1.0;
+        		ang_z = 0.0;
+        		
+        		if(ball_x < 400)
+        			ang_z = 0.02;
+        		else
+        			ang_z =-0.02;
+        			
+        		drive_robot(lin_x, ang_z);
+        		if(!(ball_x >0 && ball_x<799)){
+        		k=0;
+        		}
+
+       	}
+       	
+    }
+  
+  
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "process_image");
@@ -77,7 +134,7 @@ int main(int argc, char** argv)
 
     client = n.serviceClient<ball_chaser::DriveToTarget>("/ball_chaser/command_robot");
 
-    ros::Subscriber sub1 = n.subscribe("/camera/rgb/image_raw", 10, process_image_callback);
+    ros::Subscriber sub1 = n.subscribe("/camera/rgb/image_raw", 2, process_image_callback);
 
     ros::spin();
 
